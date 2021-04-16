e2f17cf28f01c05e997a45599f01fbef1db09f91
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-04-16 18:08:26 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,8 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
-                ],
-                [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("📣Channel", url="https://t.me/VKPROJECTS"),
+                    InlineKeyboardButton("Group💬", url="https://t.me/VKP_BOTS")
                 ]
             ]
         )
@@ -69,12 +65,8 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
-                ],
-                [
-                    InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
+                    InlineKeyboardButton("🏠BACK", callback_data="help_data"),
+                    InlineKeyboardButton("CLOSE⛔", callback_data="close_data"),
                 ]                
             ]
         )
